8a6f5b3fe3989194c2d88e854d83fc535db6c85c
Douglas Gomes <41962844+DogdyZero@users.noreply.github.com>
2019-03-01 17:57:51 -0300

    Update LivroController.java

--- src/main/java/br/com/thebooks/controller/LivroController.java
+++ src/main/java/br/com/thebooks/controller/LivroController.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import br.com.thebooks.domain.EntidadeDominio;
